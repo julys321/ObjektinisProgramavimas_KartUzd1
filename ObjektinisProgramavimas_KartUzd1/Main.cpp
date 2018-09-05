6e80f77078233f8dabd161236286b48b4a2745c3
Julius Lungys <juliuslungys@gmail.com>
2018-09-05 13:42:20 +0300

    Initial commit

---
+++ ObjektinisProgramavimas_KartUzd1/Main.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+#include <fstream>
+#include <vector>
+using namespace std;
+struct Ukininkas {
+	char registracijosNumeris[3];
+	long int parduotosBulvesKilogramais;
+};
+struct Ukininkai {
+	vector<Ukininkas> ukininkai;
+public:
+	void addUkininkas(Ukininkas ukininkas) {
+		ukininkai.push_back(ukininkas);
+	}
+	int getParduotuBulviuKilogramaisSkaicius() {
+		int parduotuBulviuKilogramaisSkaicius = 0;
+		for (Ukininkas ukininkas : ukininkai) {
+			parduotuBulviuKilogramaisSkaicius += ukininkas.parduotosBulvesKilogramais;
+		}
+		return parduotuBulviuKilogramaisSkaicius;
+	}
+	int getUkininkuSkaiciuPardavusiuVirsKilogramu(int virskKiekPardaveBulviu) {
+		int ukininkuSkaiciuPardavusiuVirsKilogramu = 0;
+		for (Ukininkas ukininkas : ukininkai) {
+			if (ukininkas.parduotosBulvesKilogramais > virskKiekPardaveBulviu)
+				ukininkuSkaiciuPardavusiuVirsKilogramu++;
+		}
+		return ukininkuSkaiciuPardavusiuVirsKilogramu;
+	}
+};
+void pildykDuomenis(Ukininkai &ukininkai);
+int main() {
+	Ukininkai ukininkai;
+	pildykDuomenis(ukininkai);
+	cout << "Kiek kilogramų bulviu buvo parduota is viso? - " << ukininkai.getParduotuBulviuKilogramaisSkaicius() << endl;
+	cout << "Kiek uzfiksuota irasu, kai buvo parduota daugiau kaip 50 kg bulviu? - " << ukininkai.getUkininkuSkaiciuPardavusiuVirsKilogramu(50) << endl;
+	system("pause");//TODO: remove
+	return 0;
+}
+void pildykDuomenis(Ukininkai &ukininkai) {
+	ifstream ukininkuDuomenysStream("ObjektinisProgramavimas_KartUzd1_Duom.txt");
+	int ukininkuKiekis;
+	ukininkuDuomenysStream >> ukininkuKiekis;
+	Ukininkas ukininkas;
+	for (int i = 0; i < ukininkuKiekis; i++) {
+		ukininkuDuomenysStream >> ukininkas.registracijosNumeris >> ukininkas.parduotosBulvesKilogramais;
+		ukininkai.addUkininkas(ukininkas);
+	}
+	ukininkuDuomenysStream.close();
+}
